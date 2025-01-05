f2963a4ec05ebefce538332768e054912f50693c
Dennis Nguyen <ngtrkhanhduy1308@gmail.com>
2025-01-05 14:44:57 +0700

    add cors config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,20 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
-    const port = configService.get('PORT');
-    await app.listen(port ?? 3000);
+
+    // Get the port and corsOrigin from config or use defaults
+    const port = configService.get<number>('PORT') ?? 3000;
+    const corsOrigin = configService.get<string>('CORS_ORIGIN') ?? 'http://localhost:3000'; // You can set this in your configuration or hard-code
+
+    app.enableCors({
+        origin: corsOrigin, // Use the corsOrigin variable here
+        methods: 'GET,POST,PUT,DELETE,OPTIONS',
+        allowedHeaders: 'Content-Type, Authorization',
+        credentials: true, // Allow cookies to be sent with requests
+    });
+
+    // Start the application
+    await app.listen(port);
 }
+
 bootstrap();
